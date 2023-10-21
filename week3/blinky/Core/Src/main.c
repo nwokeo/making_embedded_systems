dbf2c64927ae7e54dc307e5d027a0bebb1914bea
on <obiike@DESKTOP-NM33OIU>
2023-10-20 18:59:21 -0700

    use LED function, clean up

--- week3/blinky/Core/Src/main.c
+++ week3/blinky/Core/Src/main.c
@@ -8,17 +8,12 @@
 #include "main.h"
 
 /* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
-SPI_HandleTypeDef hspi1;
 PCD_HandleTypeDef hpcd_USB_FS;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
-static void MX_SPI1_Init(void);
 static void MX_USB_PCD_Init(void);
-static void MX_TIM2_Init(void);
 
 /**
   * @brief  The application entry point.
@@ -34,36 +29,37 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C1_Init();
-  MX_SPI1_Init();
   MX_USB_PCD_Init();
-  MX_TIM2_Init();
 
-  while (1)
-  {
-	  if(HAL_GPIO_ReadPin(GPIOA, B1_Pin) == GPIO_PIN_SET) {
-		  GPIOE->ODR ^= GPIO_PIN_9;
-		  HAL_Delay(1000);
-	    GPIOE->ODR ^= GPIO_PIN_10;
-	    HAL_Delay(100);
-	    GPIOE->ODR ^= GPIO_PIN_11;
-	    HAL_Delay(100);
-	    GPIOE->ODR ^= GPIO_PIN_12;
-	    HAL_Delay(100);
-	    GPIOE->ODR ^= GPIO_PIN_13;
-	    HAL_Delay(100);
-	    GPIOE->ODR ^= GPIO_PIN_14;
-	    HAL_Delay(100);
-	    GPIOE->ODR ^= GPIO_PIN_15;
-	    HAL_Delay(100);
-	    GPIOE->ODR ^= GPIO_PIN_8;
-	    HAL_Delay(100);
+  void spinLights() {
+      GPIOE->ODR ^= GPIO_PIN_8; //LD4
+      HAL_Delay(100);
+      GPIOE->ODR ^= GPIO_PIN_9; //LD3
+      HAL_Delay(100);
+      GPIOE->ODR ^= GPIO_PIN_10;
+      HAL_Delay(100);
+      GPIOE->ODR ^= GPIO_PIN_11;
+      HAL_Delay(100);
+      GPIOE->ODR ^= GPIO_PIN_12;
+      HAL_Delay(100);
+      GPIOE->ODR ^= GPIO_PIN_13;
+      HAL_Delay(100);
+      GPIOE->ODR ^= GPIO_PIN_14;
+      HAL_Delay(100);
+      GPIOE->ODR ^= GPIO_PIN_15;
+      HAL_Delay(100);
 
       GPIOE->ODR ^= GPIO_PIN_All;
       HAL_Delay(100);
       HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_All);
       HAL_Delay(100);
-	  }
+  }
+
+  while (1)
+  {
+    if(HAL_GPIO_ReadPin(GPIOA, B1_Pin) == GPIO_PIN_SET) {
+    	spinLights();
+    }
   }
 }
 
@@ -110,70 +106,6 @@ void SystemClock_Config(void)
   PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI;
   PeriphClkInit.USBClockSelection = RCC_USBCLKSOURCE_PLL;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief I2C1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_I2C1_Init(void)
-{
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x2000090E;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure Analogue filter
-  */
-  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Configure Digital filter
-  */
-  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-}
-
-/**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI1_Init(void)
-{
-  /* SPI1 parameter configuration*/
-  hspi1.Instance = SPI1;
-  hspi1.Init.Mode = SPI_MODE_MASTER;
-  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_4BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi1.Init.CRCPolynomial = 7;
-  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
-  if (HAL_SPI_Init(&hspi1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -268,20 +200,3 @@ void Error_Handler(void)
   {
   }
 }
-
-#ifdef  USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
-}
-#endif /* USE_FULL_ASSERT */
